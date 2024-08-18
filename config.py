abc9e33f901d2ce35431ee98b1ef57fb8d10d6f9
Ponce1969 <110701174+Ponce1969@users.noreply.github.com>
2024-08-18 19:37:45 -0300

    ahora separe en dos archivos el codigo y agrgegue langchain

---
+++ config.py
@@ -0,0 +1,14 @@
+import logging
+
+# Configuración de la API key
+API_KEY_FILE = "api_key.txt"
+
+# Configuración del modelo
+MODEL_NAME = 'llama-3.1-70b-versatile'
+
+# Configuración de la memoria conversacional
+CONVERSATIONAL_MEMORY_LENGTH = 6
+
+# Configuración del logger
+LOG_LEVEL = logging.ERROR
+LOG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
--- llama_llama.py
+++ llama_llama.py
@@ -1,91 +1,99 @@
-import requests
-import json
 import logging
 import sys
 from colorama import init, Fore, Style
 import textwrap
+from langchain.chains import LLMChain
+from langchain_core.prompts import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    MessagesPlaceholder,
+)
+from langchain_core.messages import SystemMessage
+from langchain.chains.conversation.memory import ConversationBufferWindowMemory
+from langchain_groq import ChatGroq
+import config  # Importar el archivo de configuración
 
 # Inicializa colorama
 init(autoreset=True)
 
-# Configuración de la API key
-api_key_file = "api_key.txt"
-
 # Configuración del logger
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.ERROR)
+logger.setLevel(config.LOG_LEVEL)
 handler = logging.StreamHandler()
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter(config.LOG_FORMAT)
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
-# Define un manejo de errores personalizado
 def handle_error(error, exit=True):
     logger.error(f"Error: {error}")
     print(f"Error: {error}")
     if exit:
         sys.exit(1)
 
-# Define una función para validar la respuesta de la API
-def validate_response(response):
-    if response.status_code == 200:
-        objeto = response.json()
-        return objeto["choices"][0]["message"]["content"]
-    else:
-        handle_error(f"Error: {response.status_code} - {response.text}")
-        return None
-    
+def format_response(response):
+    # Ajustar el texto a un ancho de 90 caracteres y agregar colores
+    wrapped_response = textwrap.fill(response,replace_whitespace=False, width=100)
+    formatted_response = f"{Fore.GREEN}{Style.BRIGHT}Respuesta de LLama 3.1:{Style.RESET_ALL}\n\n{Fore.CYAN}{wrapped_response}{Style.RESET_ALL}"
+    return formatted_response
 
-# Main function
 def main():
+    """
+    Función principal que inicializa un chatbot utilizando el modelo LLama 3.1 e interactúa con el usuario.
+    La función realiza los siguientes pasos:
+    1. Lee la clave de la API desde un archivo.
+    2. Crea una instancia de la clase ChatGroq con la clave de la API y el nombre del modelo.
+    3. Configura el sistema de inicio y la memoria conversacional.
+    4. Entra en un bucle para recibir preguntas del usuario y generar respuestas utilizando LLama 3.1.
+    5. Imprime la respuesta en la consola.
+    Parámetros:
+        Ninguno
+    Retorna:
+        Ninguno
+    """
+    
     try:
-        with open(api_key_file, 'r') as f:
+        with open(config.API_KEY_FILE, 'r') as f:
             api_key = f.read().strip()
     except FileNotFoundError:
         handle_error("La API key no fue encontrada en el archivo especificado.")
         return
 
-    # Texto de entrada para LLama 3.1
-    input_text = input("¿Que quieres preguntarle a LLama 3? : ")
+    groq_chat = ChatGroq(
+        groq_api_key=api_key, 
+        model_name=config.MODEL_NAME
+    )
 
-    # Convierte el texto de entrada a JSON
-    input_data = {
-        "messages": [
-            {
-                "role": "system",
-                "content": "Eres un experto en Python y un ingeniero en software con un gran conocimiento en este lenguaje, estas trabajando en un proyecto de desarrollo de software en python y debes entrenar a los juniors que consultan contigo."
-            },
-            {
-                "role": "user",
-                "content": input_text
-            }
-        ],
-        "model": "llama-3.1-70b-versatile"
-    }
-   
-    # Encabezados de la solicitud API
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {api_key}"
-    }
+    system_prompt = "Eres un experto en Python y un ingeniero en software con un gran conocimiento en este lenguaje, estas trabajando en un proyecto de desarrollo de software en python y debes entrenar a los juniors que consultan contigo."
     
-    try:
-        # Realizar la solicitud API
-        response = requests.post('https://api.groq.com/openai/v1/chat/completions', json=input_data, headers=headers)
+    memory = ConversationBufferWindowMemory(k=config.CONVERSATIONAL_MEMORY_LENGTH, memory_key="chat_history", return_messages=True)
 
-        # Validate the response
-        response_json = validate_response(response)
+    while True:
+        user_question = input("¿Qué quieres preguntarle a LLama 3? (escribe 'salir' para terminar): ")
+        if user_question.lower() in ['salir', 'exit']:
+            print("Saliendo del chat. ¡Hasta luego!")
+            break
 
-        if response_json:
-            # Formatear la respuesta con Markdown y colorama
-            wrapped_response = textwrap.fill(response_json, width=90)
-            formatted_response = f"{Fore.GREEN}{Style.BRIGHT}Respuesta de LLama 3.1:{Style.RESET_ALL}\n\n{Fore.CYAN}{wrapped_response}{Style.RESET_ALL}"
-            print(formatted_response, end='\n')
-        else:
-            handle_error("Error: La respuesta no es válida")
+        prompt = ChatPromptTemplate.from_messages(
+            [
+                SystemMessage(content=system_prompt),
+                MessagesPlaceholder(variable_name="chat_history"),
+                HumanMessagePromptTemplate.from_template("{human_input}"),
+            ]
+        )
 
-    except requests.exceptions.RequestException as e:
-        handle_error(f"Error al realizar la solicitud API: {e}")
+        conversation = LLMChain(
+            llm=groq_chat,
+            prompt=prompt,
+            verbose=False,
+            memory=memory,
+        )
+
+        response = conversation.predict(human_input=user_question)
+        formatted_response = format_response(response)
+        print(formatted_response, end='\n\n')
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except Exception as e:
+        handle_error(f"Error inesperado: {e}") 
